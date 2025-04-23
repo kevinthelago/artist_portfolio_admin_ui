b99f0ab3e75389e1907d6cc2582e447d2ae1f41e
Kevin Lago <kevinthelago@gmail.com>
2025-04-23 18:31:42 -0400

    Update Functions and Editable Values

--- src/App.js
+++ src/App.js
@@ -3,7 +3,60 @@ import Album from './components/Album';
 import './app.css';
 import './reset.css';
 
+const EditableValue = (props) => {
+    const [value, setValue] = useState(props.value);
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    return (
+        <input autoFocus onChange={(e) => handleChange(e)} value={value} className='artist-card-value editable-value'>
+
+        </input>
+    )
+};
+
+const NoneEditableValue = (props) => (
+    <div className='artist-card-value none-editable-value'>
+        {props.value}
+    </div>
+);
+
+const ArtistDetail = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleClick = () => {
+        setIsEditing(true);
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            setIsEditing(false);
+            props.updateArtist();
+        }
+    }
+
+    return (
+        <div
+            onKeyDown={(e) => { handleKeyDown(e) }}
+            onClick={() => { handleClick() }}
+            id={"artist-card-" + props.title.toLowerCase()}
+        >
+            <div className='artist-card-key'>
+                {props.title}
+            </div>
+            {isEditing ?
+                <EditableValue value={props.value} /> :
+                <NoneEditableValue value={props.value} />
+            }
+        </div>
+    )
+}
+
 const App = () => {
+    const hiddenProperties = ["uuid", "albums"]
+    const [value, setValue] = useState(0);
     const [artist, setArtist] = useState({
         albums: [],
         email: "",
@@ -13,29 +66,90 @@ const App = () => {
     })
 
     useEffect(() => {
-        const fetchArtist = async () => {
-            fetch(
-                process.env.REACT_APP_ARTISTS_URL + 
-                process.env.REACT_APP_ARTIST_UUID
-            )
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data)
-                    setArtist(data)
-                })
-                .catch(error => {
-                    console.log(error);
-                })
-        }
-
-        fetchArtist();
+        fetch(
+            process.env.REACT_APP_ARTISTS_URL +
+            process.env.REACT_APP_ARTIST_UUID
+        ).then(response => 
+            response.json()
+        ).then(data => {
+            setArtist(data)
+        }).catch(error => {
+            console.log(error);
+        })
     }, []);
+
+    const updateArtist = () => {
+        fetch(
+            process.env.REACT_APP_ARTISTS_URL + 
+            artist.uuid,
+            {
+                method: 'PUT',
+                body: JSON.stringify(artist)
+            }
+        ).then(response => 
+            response.json()
+        ).then(data => 
+            setArtist(data)
+        ).catch(error => 
+            console.log(error)
+        )
+    }
+
+    const createAlbum = () => {
+        fetch(
+            process.env.REACT_APP_ALBUMS_URL +
+            process.env.REACT_APP_ARTIST_UUID,
+            {
+                method: 'POST',
+                headers: {
+                    "Accept": "application/json"
+                }
+            }
+        ).then(response => 
+            response.json()
+        ).then(data => {
+            artist.albums.push(data);
+            setArtist(artist);
+            setValue(value => value + 1);
+        }).catch(error => 
+            console.log(error)
+        );
+    }
+
+    const deleteAlbum = (uuid) => {
+        fetch(
+            process.env.REACT_APP_ALBUMS_URL + uuid,
+            {
+                method: 'DELETE'
+            }
+        ).then(response => {
+            artist.albums = artist.albums.filter(album => album.uuid !== uuid);
+            setArtist(artist);
+            setValue(value => value - 1);
+        })
+    }
 
     return (
         <div id='app'>
-            {artist.albums ? artist.albums.map(album => 
-                <Album key={album.name} album={album} />
-            ) : ""}
+            <div id='artist-card'>
+                {Object.getOwnPropertyNames(artist).map(property => {
+                    if (hiddenProperties.includes(property)) return "";
+                    return <ArtistDetail
+                        key={property}
+                        title={property}
+                        value={artist[property]}
+                        updateArtist={updateArtist}
+                    />
+                })}
+            </div>
+            <div id='albums'>
+                {artist.albums?.map(album =>
+                    <Album key={album.uuid} hiddenProperties={hiddenProperties} album={album} deleteAlbum={deleteAlbum}/>
+                )}
+                <button onClick={() => createAlbum()}>
+                    Add Album
+                </button>
+            </div>
         </div>
     );
 }
--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -2,6 +2,56 @@ import { useEffect, useState } from "react";
 import Piece from './Piece';
 import './album.css';
 
+const EditableValue = (props) => {
+    const [value, setValue] = useState(props.value);
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    return (
+        <input autoFocus onChange={(e) => handleChange(e)} value={value} className='artist-card-value editable-value'>
+
+        </input>
+    )
+};
+
+const NoneEditableValue = (props) => (
+    <div className='artist-card-value none-editable-value'>
+        {props.value}
+    </div>
+);
+
+const AlbumDetail = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleClick = () => {
+        setIsEditing(true);
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            setIsEditing(false);
+        }
+    }
+
+    return (
+        <div
+            onKeyDown={(e) => { handleKeyDown(e) }}
+            onClick={() => { handleClick() }}
+            id={"album-card-" + props.title.toLowerCase()}
+        >
+            <div className='album-card-key'>
+                {props.title}
+            </div>
+            {isEditing ?
+                <EditableValue value={props.value} /> :
+                <NoneEditableValue value={props.value} />
+            }
+        </div>
+    )
+}
+
 const Album = (props) => {
     let [isOpen, setIsOpen] = useState(false)
 
@@ -11,15 +61,21 @@ const Album = (props) => {
 
     return(
         <div className={isOpen ? "album open" : "album"} onClick={() => open()}>
-            <div className="album-name">
-                {props.name}
-            </div>
-            <div className="album-index">
-                {props.i}
-            </div>
-            {props.pieces ? isOpen ? props.pieces.map(piece => 
+            {isOpen ? "" :     
+            <div className='artist-card-value none-editable-value'>
+                {props.album.name}
+            </div>}
+            {isOpen ? Object.getOwnPropertyNames(props.album).map(property => {
+                if (props.hiddenProperties.includes(property)) return "";
+                return <AlbumDetail
+                    key={property}
+                    title={property}
+                    value={props.album[property]}
+                />
+            }) : ""}
+            {isOpen ? props.pieces?.map(piece => 
                 <Piece piece={piece} />
-            ) : "" : ""}
+            ) : ""}
         </div>
     )
 }
